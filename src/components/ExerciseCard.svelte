55f9df49de3a6c313afdaec8e76d98513c3d2020
MarioMunilla <mario@LAPTOP-1J8VJVST4.com>
2025-05-03 18:49:09 +0200

    componentes: menú navegación y tarjetas con los ejercicios

---
+++ src/components/ExerciseCard.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+    type ExerciseCardProps = {
+        name: string,
+        bodyParts: string[],
+        imgURL: string 
+    }
+
+    let { name, bodyParts, imgURL }: ExerciseCardProps = $props()
+</script>
+
+<article class="exercise-card">
+	<h1 class="exercise-card__name">{name}</h1>
+	<div class="exercise-card__body-parts">Grupo muscular: {bodyParts.join(', ')}</div>
+	<img class="exercise-card__img" src={imgURL} alt={name} width="200" />
+</article>
+
+<style>
+    .exercise-card {
+        border: 0.2rem solid #f44;
+    }
+</style>
---
+++ src/components/MenuNavegacion.svelte
@@ -0,0 +1,36 @@
+<script>
+    export let items= {}
+  
+    const entradas=Object.entries(items)
+    // [["/entrenamiento","Entrenamiento"],    
+  </script>
+  
+  {#each entradas as [url,texto]}
+  <nav>
+    <a href={url}>{texto}</a>
+  </nav>
+  {/each}
+  
+    
+    <style>
+      nav {
+        background-color: #333; 
+        box-shadow: 0px 2px 5px rgba(0, 0, 0, 0.1);
+        display: flex; 
+        justify-content: space-around;
+        flex-direction: row;
+        padding: 5px 0;
+      }
+    
+      nav a {
+        color: #fff; 
+        text-decoration: none; 
+        transition: color 0.3s; 
+        border-radius: 5px; 
+        transition: background-color 0.3s; 
+      }
+    
+      nav a:hover {
+        color: #b7b7b7; 
+      }
+    </style>
