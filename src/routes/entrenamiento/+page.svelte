f5cbc0953a6d4fb8a4a365a9e18c656af4279fa5
MarioMunilla <mario@LAPTOP-1J8VJVST4.com>
2025-05-03 18:56:31 +0200

    se muestra lista de grupos musculares y ejercicios

---
+++ src/routes/entrenamiento/+page.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import ExerciseCard from '../../components/ExerciseCard.svelte';
+
+  export let data;
+  </script>
+  
+  <section>
+    <h1>Partes del cuerpo</h1>
+    <ul>
+      {#each data.bodyParts as bodyPart}
+        <li>{bodyPart.name}</li>
+      {/each}
+    </ul>
+  </section>
+
+  <h1>Lista de Ejercicios</h1>
+  
+
+  {#each data.exercises as exercise}
+    <ExerciseCard 
+      name={exercise.name} 
+      bodyParts={[exercise.bodyPart]} 
+      imgURL={exercise.gifUrl}
+    />
+  {/each}
+  
