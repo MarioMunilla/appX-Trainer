962a829631df9f0c7354945c4416b0377c79e54f
MarioMunilla <mario@LAPTOP-1J8VJVST4.com>
2025-05-14 08:55:17 +0200

    feat(components): Se desarrolló un componente genérico de filtro que permite seleccionar una opción desde un desplegable

---
+++ src/components/filter/Filter.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+    type FilterOption = {value: string, text: string}
+
+    type FilterProps = {
+		label: string,
+        options: FilterOption[],
+        onChange?: (newValue: string) => void
+	}
+
+    let { label, options, onChange }: FilterProps = $props()
+
+	let selected = $state('');
+	
+	function handleChange(event: Event) {
+        if (!onChange) { return }
+
+        const selectElement = event.target as HTMLSelectElement
+        onChange( selectElement.value )
+	}
+</script>
+
+<div class="filter">
+	<label for="dificultad">{label}</label>
+	<select id="dificultad" onchange={handleChange} bind:value={selected}>
+		{#each options as option (option.value)}
+			<option value={option.value}>{option.text}</option>
+		{/each}
+	</select>
+</div>
