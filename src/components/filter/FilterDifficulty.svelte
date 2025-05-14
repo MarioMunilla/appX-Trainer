5dd552fef0113b50f2f5b1397273ba458b6d3e9a
MarioMunilla <mario@LAPTOP-1J8VJVST4.com>
2025-05-14 08:57:06 +0200

    feat(components): utiliza el componente genérico Filter. Permite seleccionar el nivel de dificultad

---
+++ src/components/filter/FilterDifficulty.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import Filter from "./Filter.svelte";
+
+	type FilterProps = {
+        onChange?: (newValue: string) => void
+	}
+	
+    let { onChange }: FilterProps = $props()
+</script>
+
+<Filter label='Difficulty' options={[
+	{value: 'facil', text: 'Beginner'},
+	{value: 'intermedio', text: 'Intermediate'},
+	{value: 'dificil', text: 'Advanced'}
+]} {onChange} />
