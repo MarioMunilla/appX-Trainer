8c665ef1c268a80a809ecb3ae91804811f729b04
MarioMunilla <mario@LAPTOP-1J8VJVST4.com>
2025-05-14 09:03:00 +0200

    feat(components): Permite filtrar ejercicios por grupo muscular

---
+++ src/components/filter/FilterGroup.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import { createEventDispatcher } from 'svelte';
+	export let options: string[] = [];
+	let selected = '';
+	const dispatch = createEventDispatcher();
+
+	function handleChange(event: Event) {
+		const target = event.target as HTMLSelectElement;
+		selected = target.value;
+		dispatch('change', selected);
+		console.log(selected)
+	}
+</script>
+
+<div class="filter">
+	<label for="grupo">Group</label>
+	<select id="grupo" on:change={handleChange} bind:value={selected}>
+		<option value="">All</option>
+		{#each options as option}
+			<option value={option}>{option}</option>
+		{/each}
+	</select>
+</div>
