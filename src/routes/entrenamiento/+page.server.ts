bda0f1b9322f66584af1347dd69b0e8d65c5279d
MarioMunilla <mario@LAPTOP-1J8VJVST4.com>
2025-05-14 09:15:58 +0200

    feat(api): Se implementa el repositorio para cargar los ejercicios y grupos musculares desde los endpoints '/api/exercises' y '/api/body_parts'. Se maneja la paginación utilizando los queryParams de la URL.

--- src/routes/entrenamiento/+page.server.ts
+++ src/routes/entrenamiento/+page.server.ts
@@ -1,9 +1,21 @@
+import type { PageServerLoad } from './$types';
 
-export async function load({fetch}) {
-	const exercisesResponse = await fetch('/api/exercises')
-	const exercises = await exercisesResponse.json()
-	
-	const bodyPartsResponse = await fetch('/api/body-parts')
-	const bodyParts = await bodyPartsResponse.json()
-	return { exercises, bodyParts };
-}
+export const load: PageServerLoad = async ({ fetch, url }) => {
+	const [exercisesResponse, bodyPartsResponse] = await Promise.all([
+		fetch('/api/exercises?' + url.searchParams),
+		fetch('/api/body_parts')
+	]);
+
+	if (!exercisesResponse.ok || !bodyPartsResponse.ok) {
+		throw new Error('Failed to fetch data');
+	}
+
+	const exercises = await exercisesResponse.json();
+	const bodyParts = await bodyPartsResponse.json();
+	/*  */
+
+	return {
+		exercises,
+		bodyParts
+	};
+};
