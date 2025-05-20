4cf9460f5562f8524764d59f6cd699940cdd5936
MarioMunilla <mario@LAPTOP-1J8VJVST4.com>
2025-05-20 17:28:41 +0200

    feat(components): filtro para añadir a favoritos

---
+++ src/components/filter/FilterFavorites.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import { createEventDispatcher } from 'svelte';
+	let checked = false;
+	const dispatch = createEventDispatcher();
+
+	function handleToggle(event: Event) {
+        const target = event.target as  HTMLInputElement
+		checked = target.checked;
+		dispatch('change', checked);
+	}
+</script>
+
+<div class="filter">
+	<label>
+		<input type="checkbox" on:change={handleToggle} bind:checked={checked} />
+		Favorites
+	</label>
+</div>
