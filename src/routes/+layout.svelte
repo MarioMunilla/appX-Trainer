fba503bfdaf8541ddc647e76523eacf151ace55d
MarioMunilla <mario@LAPTOP-1J8VJVST4.com>
2025-05-20 17:33:03 +0200

    feat(layouts): estructura base del layout con Header, Footer y renderizado dinámico de contenido

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+    import type { Snippet } from 'svelte';
+    import type { LayoutData } from './$types';
+	import MenuNavegacion from '../components/MenuNavegacion.svelte';
+	import Footer from '../components/Footer.svelte';
+	import Header from '../components/Header.svelte';
+
+    let { data, children }: { data: LayoutData, children: Snippet } = $props();
+</script>
+
+<Header />
+
+<main>
+    {@render children()}
+</main>
+
+<Footer />
