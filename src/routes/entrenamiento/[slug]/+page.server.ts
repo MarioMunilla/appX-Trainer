6eb58ad22140288c7ec6b2f31cb34a3ef4558989
MarioMunilla <mario@LAPTOP-1J8VJVST4.com>
2025-05-22 11:46:18 +0200

    feat(api): construcción de URL para fetch de ejercicios

---
+++ src/routes/entrenamiento/[slug]/+page.server.ts
@@ -0,0 +1,20 @@
+import { error } from '@sveltejs/kit';
+import type { PageServerLoad } from './$types';
+
+export const load: PageServerLoad = async ({ params, fetch, url }) => {
+	const [exercisesResponse, bodyPartsResponse] = await Promise.all([
+		fetch('/api/exercises?' + url.searchParams + params.slug),
+		fetch('/api/body_parts')
+	]);
+
+	if (!exercisesResponse.ok || !bodyPartsResponse.ok) {
+		throw error(500, 'Fallo al cargar datos de entrenamiento');
+	}
+
+	const exercises = await exercisesResponse.json();
+	const bodyParts = await bodyPartsResponse.json();
+	return {
+		exercises,
+		bodyParts
+	};
+};
