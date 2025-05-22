4cff05ca1a6ba90de2d700523b2e86e0c05f7b00
MarioMunilla <mario@LAPTOP-1J8VJVST4.com>
2025-05-22 10:14:26 +0200

    feat(api): se agrega búsqueda por nombre y filtrado por grupo al endpoint de ejercicios; conserva paginación

--- src/routes/api/exercises/+server.ts
+++ src/routes/api/exercises/+server.ts
@@ -5,15 +5,34 @@ import { supabase } from '$lib/supabaseClient';
 const RESULTS_PER_PAGE = 6;
 
 export const GET: RequestHandler = async ({ url }) => {
-	const builder = supabase.from('exercises').select('*');
+	const page = Number(url.searchParams.get('p') ?? '0');
+	const search = url.searchParams.get('q') ?? '';
+	const group = url.searchParams.get('group');
+	
+	// let count = obtener la cuenta con .count o investigar por alguna opción mejor
 
-	const page = url.searchParams.get('p') ?? 0;
+	let builder = supabase.from('exercises').select('*').ilike('name', `%${search}%`)
+	builder.throwOnError()
 
-	builder.range(
-		Number(page) * RESULTS_PER_PAGE,
-		Number(page) * RESULTS_PER_PAGE + RESULTS_PER_PAGE - 1
-	);
+	if (group) {
+		builder = builder.eq('body_part', group);
+		console.log(builder);
+	}
 
-	const { data } = await builder;
+
+	builder = builder.range(page * RESULTS_PER_PAGE, page * RESULTS_PER_PAGE + RESULTS_PER_PAGE - 1);
+
+	const { data, error } = await builder;
+
+	if (error) {
+		return json({ error: error.message }, { status: 500 });
+	}
+
+	// const info = {
+	// 	count,
+	// 	page,
+
+	// }
+	// const results = data
 	return json(data);
 };
