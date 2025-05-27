9c8d260c0b97e76c5b29ca7df6c027dc251793fe
MarioMunilla <mario@LAPTOP-1J8VJVST4.com>
2025-05-27 15:24:55 +0200

    refactor(components): se reutiliza comoponente genérico <Filter /> y se usa el ID para filtrar por grupo muscular

--- src/components/filter/FilterGroup.svelte
+++ src/components/filter/FilterGroup.svelte
@@ -1,44 +1,14 @@
 <script lang="ts">
-	export let options: string[] = [];
+	import Filter from "./Filter.svelte";
+
+	export let options: {name: string, id: string}[] = [];
 	export let onChange: (newValue: string) => void = () => {};
-
-	let selected = '';
-
-	function handleChange(event: Event) {
-		const target = event.target as HTMLSelectElement;
-		selected = target.value;
-		onChange(selected); 
-	}
 </script>
 
-<div class="filter">
-	<label for="grupo">Group</label>
-	<select id="grupo" on:change={handleChange} bind:value={selected}>
-		<option value="">All</option>
-		{#each options as option (option)}
-			<option value={option}>{option}</option>
-		{/each}
-	</select>
-</div>
+<Filter label='Group' options={
+	options.map(option => ({text: option.name, value: option.id}))
+} id="group" {onChange} />
 
 <style>
-
-	.filter {
-		display: flex;
-		align-items: center;
-		gap: 1rem;
-		width: 100%;
-		max-width: 30rem;
-		margin-bottom: 1rem;
-	}
-
-	select {
-		flex: 1;
-		padding: 0.8rem 1rem;
-		font-size: 1rem;
-		border: 1px solid #ccc;
-		border-radius: 0.6rem;
-		background-color: #fff;
-		transition: border-color 0.2s ease;
-	}
+	
 </style>
