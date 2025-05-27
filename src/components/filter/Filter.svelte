a5ce224ee161e5d6daba417d56a72c2456b9fdeb
MarioMunilla <mario@LAPTOP-1J8VJVST4.com>
2025-05-27 16:30:29 +0200

    feat(components):  opción seleccionada por defecto en componente Filter mediante prop selected

--- src/components/filter/Filter.svelte
+++ src/components/filter/Filter.svelte
@@ -1,30 +1,28 @@
 <script lang="ts">
-    type FilterOption = {value: string, text: string}
+    type FilterOption = { value: string; text: string };
 
     type FilterProps = {
-		id: string,
-		label: string,
-        options: FilterOption[],
-        onChange?: (newValue: string) => void
-	}
+        id: string;
+        label: string;
+        options: FilterOption[];
+        selected?: string;
+        onChange?: (newValue: string) => void;
+    };
 
-    let { label, options, id, onChange }: FilterProps = $props()
+    let { label, options, id, selected = '', onChange }: FilterProps = $props();
 
-	let selected = $state('');
-	
-	function handleChange(event: Event) {
-        if (!onChange) { return }
-
-        const selectElement = event.target as HTMLSelectElement
-        onChange( selectElement.value )
-	}
+    function handleChange(event: Event) {
+        if (!onChange) return;
+        const selectElement = event.target as HTMLSelectElement;
+        onChange(selectElement.value);
+    }
 </script>
 
 <div class="filter">
-	<label for={id}>{label}</label>
-	<select id={id} onchange={handleChange} bind:value={selected}>
-		{#each options as option (option.value)}
-			<option value={option.value}>{option.text}</option>
-		{/each}
-	</select>
+    <label for={id}>{label}</label>
+    <select id={id} onchange={handleChange} bind:value={selected}>
+        {#each options as option (option.value)}
+            <option value={option.value}>{option.text}</option>
+        {/each}
+    </select>
 </div>
