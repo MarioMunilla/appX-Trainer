b1096fd6e1783bc04e4f12323f4f360d08d6fa90
MarioMunilla <mario@LAPTOP-1J8VJVST4.com>
2025-05-27 16:34:19 +0200

    feat(components): añadir opción 'All' y prop selected por defecto en filtro de dificultad

--- src/components/filter/FilterDifficulty.svelte
+++ src/components/filter/FilterDifficulty.svelte
@@ -1,15 +1,23 @@
 <script lang="ts">
-	import Filter from "./Filter.svelte";
+    import Filter from './Filter.svelte';
 
-	type FilterProps = {
-        onChange?: (newValue: string) => void
-	}
-	
-    let { onChange }: FilterProps = $props()
+    type FilterProps = {
+        onChange?: (newValue: string) => void;
+        selected?: string;
+    };
+
+    let { onChange, selected = 'all' }: FilterProps = $props();
 </script>
 
-<Filter label='Difficulty' options={[
-	{value: 'beginner', text: 'Beginner'},
-	{value: 'intermediate', text: 'Intermediate'},
-	{value: 'advanced', text: 'Advanced'}
-]} id="difficulty" {onChange} />
+<Filter
+    label="Difficulty"
+    options={[
+        { value: 'all', text: 'All' },
+        { value: 'beginner', text: 'Beginner' },
+        { value: 'intermediate', text: 'Intermediate' },
+        { value: 'advanced', text: 'Advanced' }
+    ]}
+    id="difficulty"
+    {onChange}
+    selected={selected}
+/>
