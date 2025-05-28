133ef36e37d95ec6aea24742fd6656d6ed4a8ad4
MarioMunilla <mario@LAPTOP-1J8VJVST4.com>
2025-05-28 17:33:37 +0200

    feat(api): Reemplazado fetch múltiple de ejercicios por endpoint específico - Simplificado parámetros de búsqueda

--- src/routes/entrenamiento/[slug]/+page.server.ts
+++ src/routes/entrenamiento/[slug]/+page.server.ts
@@ -1,20 +1,21 @@
 import { error } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 
-export const load: PageServerLoad = async ({ params, fetch, url }) => {
-	const [exercisesResponse, bodyPartsResponse] = await Promise.all([
-		fetch('/api/exercises?' + url.searchParams + params.slug),
+export const load: PageServerLoad = async ({ params, fetch }) => {
+	const [exerciseResponse, bodyPartsResponse] = await Promise.all([
+		fetch(`/api/exercises/${params.slug}`),
 		fetch('/api/body_parts')
 	]);
 
-	if (!exercisesResponse.ok || !bodyPartsResponse.ok) {
+	if (!exerciseResponse.ok || !bodyPartsResponse.ok) {
 		throw error(500, 'Fallo al cargar datos de entrenamiento');
 	}
 
-	const exercises = await exercisesResponse.json();
+	const exercise = await exerciseResponse.json();
 	const bodyParts = await bodyPartsResponse.json();
+
 	return {
-		exercises,
+		exercise,
 		bodyParts
 	};
 };
