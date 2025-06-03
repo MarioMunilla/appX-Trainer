672bf886aaac0a527971599dc0cea4bf7fe8dd57
MarioMunilla <mario@LAPTOP-1J8VJVST4.com>
2025-06-03 11:12:47 +0200

    feat(pages): Cuando data.exercises.results.length === 0, verás el mensaje 'No se encontraron ejercicios para tu búsqueda' en lugar del grid vacío

--- src/routes/entrenamiento/+page.svelte
+++ src/routes/entrenamiento/+page.svelte
@@ -1,220 +1,224 @@
 <script lang="ts">
-    import { page } from '$app/stores';
-    import SearchBar from '../../components/SearchBar.svelte';
-    import type { PageData } from './$types';
-    import ExerciseCard from '../../components/ExerciseCard.svelte';
-    import FilterFavorites from '../../components/filter/FilterFavorites.svelte';
-    import FilterEquipment from '../../components/filter/FilterEquipment.svelte';
-    import Filter from '../../components/filter/Filter.svelte';
-    import FilterGroup from '../../components/filter/FilterGroup.svelte';
-    import { goto } from '$app/navigation';
-    import FilterDifficulty from '../../components/filter/FilterDifficulty.svelte';
-    import { supabase } from '$lib/supabaseClient';
-    import { onMount } from 'svelte';
+	import { page } from '$app/stores';
+	import SearchBar from '../../components/SearchBar.svelte';
+	import type { PageData } from './$types';
+	import ExerciseCard from '../../components/ExerciseCard.svelte';
+	import FilterFavorites from '../../components/filter/FilterFavorites.svelte';
+	import FilterEquipment from '../../components/filter/FilterEquipment.svelte';
+	import FilterGroup from '../../components/filter/FilterGroup.svelte';
+	import { goto } from '$app/navigation';
+	import FilterDifficulty from '../../components/filter/FilterDifficulty.svelte';
+	import { supabase } from '$lib/supabaseClient';
+	import { onMount } from 'svelte';
 
-    export let data: PageData;
-    
-    let selectedDifficulty = $page.url.searchParams.get('difficulty') || 'all';
-    let selectedEquipment = '';
-    let showFavorites = $page.url.searchParams.get('favorites') === 'true';
-    let searchTerm = '';
-    let searchTermGroup = '';
-    let userId: string | null = null;
-    
-    $: searchTerm = $page.url.searchParams.get('q') || '';
-    $: searchTermGroup = $page.url.searchParams.get('group') || 'all';
+	export let data: PageData;
 
-    onMount(async () => {
-        const { data, error } = await supabase.auth.getUser();
-        if (data?.user) {
-            userId = data.user.id;
-        }
-    });
+	let selectedDifficulty = $page.url.searchParams.get('difficulty') || 'all';
+	let selectedEquipment = '';
+	let showFavorites = $page.url.searchParams.get('favorites') === 'true';
+	let searchTerm = '';
+	let searchTermGroup = '';
+	let userId: string = '9844e6c1-0812-4f01-aa1b-1258abc17d65';
 
-    function handleGroupChange(group: string) {
-        const params = new URLSearchParams($page.url.searchParams);
-        params.delete('p'); 
+	$: searchTerm = $page.url.searchParams.get('q') || '';
+	$: searchTermGroup = $page.url.searchParams.get('group') || 'all';
 
-        if (group && group !== 'all') {
-            params.set('group', group);
-        } else {
-            params.delete('group');
-        }
-        goto(`/entrenamiento?${params.toString()}`);
-    }
+	function handleGroupChange(group: string) {
+		const params = new URLSearchParams($page.url.searchParams);
+		params.delete('p');
 
-    function handleLevel(level: string) {
-        selectedDifficulty = level;
-        const params = new URLSearchParams($page.url.searchParams);
-        params.delete('p'); 
+		if (group && group !== 'all') {
+			params.set('group', group);
+		} else {
+			params.delete('group');
+		}
+		goto(`/entrenamiento?${params.toString()}`);
+	}
 
-        if (level && level !== 'all') {
-            params.set('difficulty', level);
-        } else {
-            params.delete('difficulty');
-        }
+	function handleLevel(level: string) {
+		selectedDifficulty = level;
+		const params = new URLSearchParams($page.url.searchParams);
+		params.delete('p');
 
-        goto(`/entrenamiento?${params.toString()}`);
-    }
+		if (level && level !== 'all') {
+			params.set('difficulty', level);
+		} else {
+			params.delete('difficulty');
+		}
 
-    function handleFavoritesChange(checked: boolean) {
-        showFavorites = checked;
-        const params = new URLSearchParams($page.url.searchParams);
-        params.delete('p'); 
+		goto(`/entrenamiento?${params.toString()}`);
+	}
 
-        if (checked && userId) {
-            params.set('favorites', 'true');
-            params.set('user_id', userId);
-        } else {
-            params.delete('favorites');
-            params.delete('user_id');
-        }
+	async function handleFavoritesChange(e: CustomEvent<boolean>) {
+		const newValue = e.detail;
+		if (newValue && !userId) {
+			console.log(newValue);
+			const { data: userData } = await supabase.auth.getUser();
+			if (userData?.user) {
+				userId = userData.user.id;
+			} else {
+				console.warn('No se pudo obtener el userId para favoritos');
+				return;
+			}
+		}
 
-        goto(`/entrenamiento?${params.toString()}`);
-    }
+		showFavorites = newValue;
 
-    async function fetchNextPage() {
-        const nextPage = data.exercises.info.next;
-        const params = new URLSearchParams($page.url.searchParams);
-        params.set('p', nextPage?.toString() ?? '0');
+		const params = new URLSearchParams($page.url.searchParams);
 
-        const response = await fetch(`/api/exercises?${params.toString()}`);
-        const jsonResponse = await response.json();
+		if (showFavorites && userId) {
+			console.log(showFavorites);
+			params.set('favorites', 'true');
+			params.set('user_id', userId);
+		} else {
+			params.delete('favorites');
+			params.delete('user_id');
+		}
 
-        data.exercises.info = jsonResponse.info;
-        data.exercises.results = [...data.exercises.results, ...jsonResponse.results];
-    }
+		goto(`/entrenamiento?${params.toString()}`);
+	}
+
+	async function fetchNextPage() {
+		const nextPage = data.exercises.info.next;
+		const params = new URLSearchParams($page.url.searchParams);
+		params.set('p', nextPage?.toString() ?? '0');
+
+		const response = await fetch(`/api/exercises?${params.toString()}`);
+		const jsonResponse = await response.json();
+
+		data.exercises.info = jsonResponse.info;
+		data.exercises.results = [...data.exercises.results, ...jsonResponse.results];
+	}
 </script>
 
 <section class="entrenamiento">
-    <SearchBar initialQuery={searchTerm} />
+	<SearchBar initialQuery={searchTerm} />
 
-    <aside class="entrenamiento__filters">
-        <h2 class="entrenamiento__filters-title">Filters</h2>
+	<aside class="entrenamiento__filters">
+		<h2 class="entrenamiento__filters-title">Filters</h2>
 
-        <FilterGroup 
-            options={data.bodyParts} 
-            onChange={handleGroupChange} 
-            selected={searchTermGroup} 
-        />
-        
-        <FilterDifficulty 
-            selected={selectedDifficulty} 
-            onChange={handleLevel} 
-        />
+		<FilterGroup options={data.bodyParts} onChange={handleGroupChange} selected={searchTermGroup} />
 
-        <FilterEquipment on:change={(e) => (selectedEquipment = e.detail)} />
-        
-      <!--   <FilterFavorites 
-            bind:checked={showFavorites}
-            on:change={handleFavoritesChange} 
-        />  -->
-    </aside>
+		<FilterDifficulty selected={selectedDifficulty} onChange={handleLevel} />
 
-    <main class="entrenamiento__content">
-        <div class="entrenamiento__grid">
-            {#each data.exercises.results as exercise (exercise.id)}
-                <ExerciseCard
-                    name={exercise.name}
-                    bodyParts={[exercise.bodyPart]}
-                    gif_url={exercise.gif_url}
-                    difficulty={exercise.difficulty}
-                />
-            {/each}
-        </div>
+		<FilterEquipment on:change={(e) => (selectedEquipment = e.detail)} />
 
-        {#if data.exercises.info.next}
-            <button on:click={fetchNextPage} class="load-more">Cargar más</button>
-        {/if}
-    </main>
+		<FilterFavorites bind:checked={showFavorites} on:change={handleFavoritesChange} />
+	</aside>
+
+	<main class="entrenamiento__content">
+		<div class="entrenamiento__grid">
+			{#each data.exercises.results as exercise (exercise.id)}
+				<ExerciseCard
+					id={exercise.id}
+					name={exercise.name}
+					bodyParts={[exercise.bodyPart]}
+					gif_url={exercise.gif_url}
+					difficulty={exercise.difficulty}
+					isFavorite={exercise.isFavorite ?? false}
+				/>
+			{:else}
+				<p class="no-results">No se encontraron ejercicios para tu búsqueda.</p>
+			{/each}
+		</div>
+		{#if data.exercises.info.next}
+			<button on:click={fetchNextPage} class="load-more">Cargar más</button>
+		{/if}
+	</main>
 </section>
 
 <style>
-  .entrenamiento {
-	max-width: 128rem;
-	margin: 0 auto;
-	padding: 2rem;
-	display: grid;
-	grid-template-areas:
-		"search search"
-		"filters content";
-	grid-template-columns: 1fr 3fr;
-	gap: 2rem;
-}
-
-:global(.entrenamiento > :first-child) {
-	grid-area: search;
-}
-
-.entrenamiento__filters {
-	grid-area: filters;
-	background-color:#7e7979;
-	padding: 2rem;
-	border-radius: 1.2rem;
-	box-shadow: 0 0.4rem 1.2rem rgba(0, 0, 0, 0.05);
-}
-
-.entrenamiento__filters-title {
-	font-size: 1.8rem;
-	margin-bottom: 1.2rem;
-}
-
-.entrenamiento__content {
-	grid-area: content;
-	display: flex;
-	flex-direction: column;
-	gap: 2rem;
-}
-
-.entrenamiento__grid {
-	display: grid;
-	grid-template-columns: repeat(auto-fit, minmax(20rem, 1fr));
-	gap: 2rem;
-}
-
-.load-more {
-	align-self: center;
-	padding: 1rem 2rem;
-	font-size: 1.6rem;
-	background-color: #3b82f6;
-	color: #fff;
-	border: none;
-	border-radius: 0.6rem;
-	cursor: pointer;
-	transition: background-color 0.2s ease-in-out;
-}
-
-.load-more:hover {
-	background-color: #2563eb;
-}
-
-@media (max-width: 1024px) {
 	.entrenamiento {
-		grid-template-columns: 1fr;
+		max-width: 128rem;
+		margin: 0 auto;
+		padding: 2rem;
+		display: grid;
 		grid-template-areas:
-			"search"
-			"filters"
-			"content";
+			'search search'
+			'filters content';
+		grid-template-columns: 1fr 3fr;
+		gap: 2rem;
 	}
-}
 
-@media (max-width: 600px) {
-	.entrenamiento {
-		padding: 0.5rem;
+	:global(.entrenamiento > :first-child) {
+		grid-area: search;
 	}
 
 	.entrenamiento__filters {
-		padding: 1.5rem;
+		grid-area: filters;
+		background-color: #7e7979;
+		padding: 2rem;
+		border-radius: 1.2rem;
+		box-shadow: 0 0.4rem 1.2rem rgba(0, 0, 0, 0.05);
 	}
 
 	.entrenamiento__filters-title {
-		font-size: 1.6rem;
+		font-size: 1.8rem;
+		margin-bottom: 1.2rem;
 	}
 
-	.load-more {
-		width: 50%;
-        padding: auto 0 0 1em;
-		font-size: 1.4rem;
+	.entrenamiento__content {
+		grid-area: content;
+		display: flex;
+		flex-direction: column;
+		gap: 2rem;
 	}
-}
+
+	.entrenamiento__grid {
+		display: grid;
+		grid-template-columns: repeat(auto-fit, minmax(20rem, 1fr));
+		gap: 2rem;
+	}
+	.no-results {
+		grid-column: 1 / -1;
+		text-align: center;
+		font-size: 1.6rem;
+		color: #e41654;
+		padding: 2rem;
+	}
+	.load-more {
+		align-self: center;
+		padding: 1rem 2rem;
+		font-size: 1.6rem;
+		background-color: #3b82f6;
+		color: #fff;
+		border: none;
+		border-radius: 0.6rem;
+		cursor: pointer;
+		transition: background-color 0.2s ease-in-out;
+	}
+
+	.load-more:hover {
+		background-color: #2563eb;
+	}
+
+	@media (max-width: 1024px) {
+		.entrenamiento {
+			grid-template-columns: 1fr;
+			grid-template-areas:
+				'search'
+				'filters'
+				'content';
+		}
+	}
+
+	@media (max-width: 600px) {
+		.entrenamiento {
+			padding: 0.5rem;
+		}
+
+		.entrenamiento__filters {
+			padding: 1.5rem;
+		}
+
+		.entrenamiento__filters-title {
+			font-size: 1.6rem;
+		}
+
+		.load-more {
+			width: 50%;
+			padding: auto 0 0 1em;
+			font-size: 1.4rem;
+		}
+	}
 </style>
