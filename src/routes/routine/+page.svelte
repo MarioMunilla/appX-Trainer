5eb52ee31a85632792a0629bd752cf700d211b49
MarioMunilla <mario@LAPTOP-1J8VJVST4.com>
2025-06-07 20:37:04 +0200

    feat(pages): muestra lista de ejercicios de la rutina o mensaje si está vacía

---
+++ src/routes/routine/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+	import type { PageData } from './$types';
+	export let data: PageData;
+</script>
+
+<h1>Mi Rutina</h1>
+
+{#if data.exercises.length > 0}
+	<ul>
+		{#each data.exercises as item}
+			<li>{item.exercises.name}</li>
+		{/each}
+	</ul>
+{:else}
+	<p>No tienes ejercicios en tu rutina aún.</p>
+{/if}
