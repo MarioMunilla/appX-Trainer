defeee90b0c5d6c5e8566cbf68147a3f2f7a6b26
MarioMunilla <mario@LAPTOP-1J8VJVST4.com>
2025-06-09 11:14:50 +0200

    fix(pages): Se cambió el endpoint de /api/user_routines a /api/routines y se eliminó el uso de url.searchParams en la llamada fetch para simplificar la carga de datos.

--- src/routes/routine/+page.server.ts
+++ src/routes/routine/+page.server.ts
@@ -1,13 +1,10 @@
 import { error } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 
-export const load: PageServerLoad = async ({ fetch, url }) => {
+export const load: PageServerLoad = async ({ fetch }) => {
 	const [exercisesResponse] = await Promise.all([
-		fetch(
-			`/api/user_routines?user_id=9844e6c1-0812-4f01-aa1b-1258abc17d65${url.searchParams.toString()}`
-		)
+		fetch(`/api/routines?user_id=9844e6c1-0812-4f01-aa1b-1258abc17d65`)
 	]);
-
 	if (!exercisesResponse.ok) {
 		throw error(500, 'Fallo al cargar datos de exercise');
 	}
