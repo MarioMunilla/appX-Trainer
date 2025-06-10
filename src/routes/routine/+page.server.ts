22fa4dbc1ff253ad3de15a850470bae3adf9e10e
MarioMunilla <mario@LAPTOP-1J8VJVST4.com>
2025-06-10 14:56:20 +0200

    feat(api): se elimina Promise.All innecesario para una sola peticion y se extrae routine_id

--- src/routes/routine/+page.server.ts
+++ src/routes/routine/+page.server.ts
@@ -2,15 +2,16 @@ import { error } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async ({ fetch }) => {
-	const [exercisesResponse] = await Promise.all([
-		fetch(`/api/routines?user_id=9844e6c1-0812-4f01-aa1b-1258abc17d65`)
-	]);
-	if (!exercisesResponse.ok) {
+	const response = await fetch(`/api/routines?user_id=9844e6c1-0812-4f01-aa1b-1258abc17d65`);
+
+	if (!response.ok) {
 		throw error(500, 'Fallo al cargar datos de exercise');
 	}
 
-	const exercises = await exercisesResponse.json();
+	const { routine_id, exercises } = await response.json();
+
 	return {
+		routine_id,
 		exercises
 	};
 };
