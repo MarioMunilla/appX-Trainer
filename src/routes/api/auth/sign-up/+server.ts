832598da25dc4941fceb547f38b3b959d930b568
MarioMunilla <mario@LAPTOP-1J8VJVST4.com>
2025-06-13 13:23:25 +0200

    feat(api): manejo de errores para cuentas ya registradas

--- src/routes/api/auth/sign-up/+server.ts
+++ src/routes/api/auth/sign-up/+server.ts
@@ -18,7 +18,18 @@ export const POST: RequestHandler = async ({ request }) => {
 	});
 
 	if (error) {
-		return new Response(JSON.stringify({ error: error.message }), { status: 400 });
+		const message =
+			error.status === 400 && error.message.includes('already registered')
+				? 'This account is already registered'
+				: error.message;
+
+		return new Response(JSON.stringify({ error: message }), { status: 400 });
+	}
+
+	if (data.user?.identities?.length === 0) {
+		return new Response(JSON.stringify({ error: 'This account is already registered' }), {
+			status: 400
+		});
 	}
 
 	return new Response(JSON.stringify({ user: data.user }), { status: 200 });
