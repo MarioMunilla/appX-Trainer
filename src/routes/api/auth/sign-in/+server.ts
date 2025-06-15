e7ee736550712f73ba2b7c7ee0f2c48beb9dbe05
MarioMunilla <mario@LAPTOP-1J8VJVST4.com>
2025-06-15 13:49:51 +0200

    feat(api): añade manejo de cookies en login
    - Valida existencia de sesión
    - Implementa cookie HttpOnly para access_token
    - Mejora mensajes de error+

--- src/routes/api/auth/sign-in/+server.ts
+++ src/routes/api/auth/sign-in/+server.ts
@@ -9,16 +9,30 @@ type LoginRequest = {
 export const POST: RequestHandler = async ({ request }) => {
 	const { email, password } = (await request.json()) as LoginRequest;
 
-	const { data, error } = await supabase.auth.signInWithPassword({
-		email,
-		password
-	});
+	const { data, error } = await supabase.auth.signInWithPassword({ email, password });
 
-	if (error) {
-		return new Response(JSON.stringify({ error: error.message }), { status: 401 });
+	if (error || !data.session) {
+		return new Response(JSON.stringify({ error: error?.message || 'Login failed' }), {
+			status: 401
+		});
 	}
 
-	return new Response(JSON.stringify({ session: data.session, user: data.user }), {
-		status: 200
+	const accessToken = data.session.access_token;
+
+	const isDev = process.env.NODE_ENV !== 'production';
+
+	// Set cookie para el access_token
+	const cookie = `access_token=${accessToken}; Path=/; HttpOnly; ${
+		isDev ? '' : 'Secure;'
+	} SameSite=Lax; Max-Age=3600`;
+	/* 
+	console.log('Cookie set:', cookie);
+	console.log('Acces token: ', accessToken); */
+	return new Response(JSON.stringify({ user: data.user }), {
+		status: 200,
+		headers: {
+			'Set-Cookie': cookie,
+			'Content-Type': 'application/json'
+		}
 	});
 };
